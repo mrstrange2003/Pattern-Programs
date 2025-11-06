de18cee6deb9be659dd53b037914fefa1c0f5478
J Dipayan Rao <raodipayan@gmail.com>
2025-11-06 23:56:56 +0530

    Added 6-11-25 patterns

---
+++ CPP/App Patterns/6-11-25/pattern23.cpp
@@ -0,0 +1,19 @@
+/*
+1 1 1 1 1 
+0 0 0 0 0 
+1 1 1 1 1 
+0 0 0 0 0 
+1 1 1 1 1 
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++)
+            cout<<(i%2)<<" ";
+        cout<<endl;
+    }
+}
