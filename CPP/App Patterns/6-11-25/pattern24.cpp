b78ca541defe7d9790fee330d33db62e96b47b0b
J Dipayan Rao <raodipayan@gmail.com>
2025-11-06 23:59:50 +0530

    Added

---
+++ CPP/App Patterns/6-11-25/pattern24.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<n;j++){
+            cout<<(j%2)<<" ";
+
+        }
+        cout<<endl;
+    }
+}
